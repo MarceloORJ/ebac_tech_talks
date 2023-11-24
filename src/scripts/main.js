cd1287c947c13a378c43e88757087b750f4e9369
Marcelo Oliveira <marcelo@exemplo.com>
2023-11-24 12:43:40 -0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const dataDoEvento = new Date("Jul 4, 2024 00:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function(){
